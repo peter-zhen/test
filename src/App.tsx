6c298ca1273527ba5c9be32d6a7540909ee61f68
peter-zhen <peter.5.2.0.1zen@gmail.com>
2024-02-07 17:34:09 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import { PaymentProvider } from "./providers/PaymentProvider";
-import { WagmiConfigProvider } from "./providers/WagmiConfigProvider";
-import { ConnectionTypeProvider } from "./providers/ConnectionTypeProvider";
+import { PaymentProvider } from './providers/PaymentProvider'
+import { WagmiConfigProvider } from './providers/WagmiConfigProvider'
+import { ConnectionTypeProvider } from './providers/ConnectionTypeProvider'
 
-import PaymentPage from "./pages/Payment/PaymentPage";
-import HomePage from "./pages/Homepage/HomePage";
-
+import PaymentPage from './pages/Payment/PaymentPage'
+import HomePage from './pages/Homepage/HomePage'
 const App: React.FC = () => {
   return (
     <Router>
@@ -18,14 +17,14 @@ const App: React.FC = () => {
         <ConnectionTypeProvider>
           <PaymentProvider>
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/payment" element={<PaymentPage />} />
+              <Route path='/' element={<HomePage />} />
+              <Route path='/payment' element={<PaymentPage />} />
             </Routes>
           </PaymentProvider>
         </ConnectionTypeProvider>
       </WagmiConfigProvider>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
